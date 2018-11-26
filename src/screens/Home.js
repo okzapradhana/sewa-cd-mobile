d3ccac2fb3d74c2a7e819666d0384f77619b76c2
luddarell <lud.darell@gmail.com>
2018-11-26 23:24:40 +0700

    add clickable props on Card component and date handling on home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,25 +4,35 @@ import {
   Text,
   StyleSheet,
   FlatList,
-  ActivityIndicator,
+  ActivityIndicator
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { getCD } from '../controllers/CDController';
-import { getProfile } from '../controllers/UserController'
+import { getProfile } from '../controllers/UserController';
 import Card from '../components/Card';
 import { color } from '../libs/metrics';
 import FloatingButton from '../components/FloatingButton';
 import NavigationService from '../libs/NavigationService';
 import Toast from 'antd-mobile-rn/lib/toast';
-import { Button } from 'antd-mobile-rn';
+import { Button, WhiteSpace, Modal, Checkbox, InputItem } from 'antd-mobile-rn';
 import { RkButton } from 'react-native-ui-kitten';
+import DateTimePicker from 'react-native-modal-datetime-picker';
+import { addPenyewaan } from '../controllers/PenyewaanController';
+import moment from 'moment';
 
 class Home extends Component {
   state = {
     user_id: '',
+    cd_id: '',
+    date_start: '',
+    date_end: '',
     allCd: [],
     refreshData: false,
-    isLoading: true
+    isLoading: true,
+    isDateTimePickerVisible: false,
+    visible: false,
+    isChecklist: false,
+    code: ''
   };
 
   static navigationOptions = ({ navigation }) => ({
@@ -56,38 +66,53 @@ class Home extends Component {
     this.setState({ allCd: allCd, isLoading: false });
   };
 
-<<<<<<< HEAD
-  addtoCart = async () => {
-    Toast.success('Berhasil tambah cart', 2);
+  getCurrenUser = async () => {
+    const user = await getProfile();
+    console.log('Id user', user[0].id);
+    this.setState({ user_id: user[0].id });
   };
-
-  addtoPeminjaman = async () => {
-    Toast.success('Berhasil tambah Peminjaman', 2);
-  };
-=======
-  getCurrenUser = async() => {
-    const user = await getProfile()
-    console.log('Id user' , user[0].id)
-    this.setState({user_id: user[0].id})
-
-  }
->>>>>>> c4a410224c2c916c9a4aee3bfed03622a672b7a0
 
   _onPressFloatingButton = () => {
     NavigationService.navigate('AddCD');
   };
 
+  _showDateTimePicker = () => this.setState({ isDateTimePickerVisible: true });
+
+  _hideDateTimePicker = () => this.setState({ isDateTimePickerVisible: false });
+
+  onClose = () => {
+    this.setState({
+      visible: false
+    });
+  };
+
+  _handleDatePicked = date => {
+    console.log('A date has been picked: ', moment(date).format('MM-DD-YYYY'));
+    console.log('Now date', moment().format('MM-DD-YYYY'));
+    const nowDate = moment().format('YYYY-MM-DD');
+    const endDate = moment(date).format('YYYY-MM-DD');
+    this.setState({ date_start: nowDate, date_end: endDate });
+    this._hideDateTimePicker();
+  };
+
+  _addPenyewaan = () => {
+    const { user_id, cd_id, date_start, date_end } = this.state;
+    const res = await addPenyewaan(user_id, cd_id, date_start, date_end);
+  };
+
   renderContent = item => {
     return (
       <Card
+        clickable
         minHeight={100}
         title={item.name}
         extraContent={
           <View style={{ flexDirection: 'row', padding: 8 }}>
-            <RkButton rkType="primary" onClick={this.addtoCart}>
-              <Text style={{ color: 'white' }}>Buy</Text>
-            </RkButton>
-            <RkButton rkType="warning">
+            <RkButton
+              style={styles.buttonStyle1}
+              rkType="warning"
+              onPress={() => this.setState({ visible: true, cd_id: item.id })}
+            >
               <Text style={{ color: 'white' }}>Rent</Text>
             </RkButton>
           </View>
@@ -122,6 +147,54 @@ class Home extends Component {
           </View>
         )}
         <FloatingButton onPress={() => this._onPressFloatingButton()} />
+        <Modal
+          title="Rent"
+          transparent
+          onClose={this.onClose}
+          maskClosable
+          visible={this.state.visible}
+          closable
+        >
+          <View style={{ paddingVertical: 20 }}>
+            <Button type="primary" onClick={this._showDateTimePicker}>
+              Duration
+            </Button>
+            <InputItem
+              disabled
+              style={styles.input}
+              value={this.state.date_end}
+            />
+            <WhiteSpace />
+            <Checkbox
+              checked={this.state.isChecklist}
+              style={{ tintColor: '#f00' }}
+              onChange={event => {
+                this.setState({ isChecklist: event.target.checked });
+              }}
+            >
+              Use Code
+            </Checkbox>
+            {this.state.isChecklist && (
+              <InputItem
+                clear
+                style={styles.input}
+                value={this.state.code}
+                onChange={value => this.setState({ code: value })}
+                placeholder="Code"
+              >
+                Code
+              </InputItem>
+            )}
+          </View>
+          <Button type="warning" onClick={() => this._addPenyewaan()}>
+            Rent!
+          </Button>
+          <DateTimePicker
+            isVisible={this.state.isDateTimePickerVisible}
+            onConfirm={this._handleDatePicked}
+            onCancel={this._hideDateTimePicker}
+          />
+        </Modal>
       </View>
     );
   }
@@ -140,10 +213,10 @@ const styles = StyleSheet.create({
   cardContent: {
     flexDirection: 'row',
     justifyContent: 'space-around'
+  },
+  buttonStyle1: {
+    flex: 1
   }
-  // buttonStyle1: {
-  //   flex: 1
-  // },
   // buttonStyle2: {
   //   flex: 1
   // }
--- src/screens/Users.js
+++ src/screens/Users.js
@@ -1,26 +1,32 @@
-import React, { Component } from 'react'
-import { View, Text, ActivityIndicator, FlatList, StyleSheet, Image } from 'react-native'
-import { Ionicons } from '@expo/vector-icons'
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Image
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import { color } from '../libs/metrics';
 import Card from '../components/Card';
 import { getUsers } from '../controllers/UsersController';
 
 class Users extends Component {
-
   state = {
     users: [],
     refreshData: false,
     isLoading: true
-  }
+  };
 
   static navigationOptions = ({ navigation }) => ({
     title: 'Users',
     drawerLabel: 'Users',
 
     headerStyle: {
-      backgroundColor: color.primary,
+      backgroundColor: color.primary
     },
-    headerLeft:
+    headerLeft: (
       <View style={{ paddingLeft: 15 }}>
         <Ionicons
           name="md-menu"
@@ -28,77 +34,91 @@ class Users extends Component {
           color={color.white}
           onPress={() => navigation.openDrawer()}
         />
-      </View>,
-    headerTintColor: color.white,
-  })
+      </View>
+    ),
+    headerTintColor: color.white
+  });
 
-  renderContent = (item) => {
+  renderContent = item => {
     return (
-      <Card title={item.name.toUpperCase()} minHeight={150}>
+      <Card clickable={false} title={item.name.toUpperCase()} minHeight={150}>
         <View style={{ display: 'flex', flexDirection: 'row' }}>
           <View style={styles.photo}>
-            <Image style={styles.profilePhoto} source={require("../../assets/user.png")} />
+            <Image
+              style={styles.profilePhoto}
+              source={require('../../assets/user.png')}
+            />
           </View>
           <View style={styles.detailProfile}>
-            <View style={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'column' }}>
-              <Text style={{fontWeight: 'bold'}}>Address</Text>
+            <View
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                flexDirection: 'column'
+              }}
+            >
+              <Text style={{ fontWeight: 'bold' }}>Address</Text>
               <Text>{item.alamat === '' ? '-' : item.alamat}</Text>
             </View>
-            <View style={{ marginTop: 20, display: 'flex', justifyContent: 'space-between', flexDirection: 'column' }}>
-              <Text style={{fontWeight: 'bold'}}>Phone</Text>
+            <View
+              style={{
+                marginTop: 20,
+                display: 'flex',
+                justifyContent: 'space-between',
+                flexDirection: 'column'
+              }}
+            >
+              <Text style={{ fontWeight: 'bold' }}>Phone</Text>
               <Text>{item.phone}</Text>
             </View>
           </View>
         </View>
       </Card>
-    )
-  }
+    );
+  };
 
   _getUsers = async () => {
-    const users = await getUsers()
-    console.log('Users data', users)
-    this.setState({ users, isLoading: false })
-  }
+    const users = await getUsers();
+    console.log('Users data', users);
+    this.setState({ users, isLoading: false });
+  };
 
   componentDidMount = async () => {
-    await this._getUsers()
-  }
+    await this._getUsers();
+  };
 
   render() {
-    const { isLoading, users, refreshData } = this.state
+    const { isLoading, users, refreshData } = this.state;
     return (
       <View style={styles.container}>
         {isLoading ? (
           <View style={styles.loadingIndicator}>
-            <ActivityIndicator
-              size="large"
-              color={color.primary}
-            />
+            <ActivityIndicator size="large" color={color.primary} />
           </View>
         ) : (
-            <FlatList
-              contentContainerStyle={{ padding: 8 }}
-              refreshing={refreshData}
-              onRefresh={() => this._getUsers()}
-              data={users}
-              renderItem={({ item }) => this.renderContent(item)}
-              keyExtractor={item => item.id.toString()}
-            />
-          )}
+          <FlatList
+            contentContainerStyle={{ padding: 8 }}
+            refreshing={refreshData}
+            onRefresh={() => this._getUsers()}
+            data={users}
+            renderItem={({ item }) => this.renderContent(item)}
+            keyExtractor={item => item.id.toString()}
+          />
+        )}
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 10,
+    padding: 10
   },
   loadingIndicator: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   photo: {
     flex: 1,
@@ -116,6 +136,6 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'center'
   }
-})
+});
 
-export default Users
+export default Users;
